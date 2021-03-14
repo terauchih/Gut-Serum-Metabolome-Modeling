1eaf85555a9875eaaaab49509131c1d18807afb3
Hinako Terauchi <terauch1@msu.edu>
2021-03-13 22:31:46 -0500

    committing stub function files

---
+++ network_analysis.py
@@ -0,0 +1,25 @@
+def match_pathways_with_taxa(taxa, pathways):
+    """
+    matches appropriate pathways to the inputted taxa.
+    
+    
+    """
+    
+    # find out which pathways each bacteria in taxa have
+    
+    # match all the pathways for each bacteria
+    
+    return "probably a dataframe with all appropriate pathways for each bacteria"
+
+
+def network_analysis(df_from_matched_pathways):
+    """
+    Runs the networkd analysis for the metabolic pathways of bacteria in group.
+    """
+    
+    # networkx
+    
+    # network analysis no graphin
+    
+    return "result of network analysis"
+    
---
+++ plot_results.py
@@ -0,0 +1,11 @@
+def plot_network_analysis(network_analysis_result):
+    """
+    Plots the result from network_analysis().
+    
+    """
+    
+    # matplotlib
+    
+    # plot
+    
+    # just display no return 
