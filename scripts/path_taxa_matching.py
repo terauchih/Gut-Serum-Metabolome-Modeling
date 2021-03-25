6c7a108ef833ab252a730c5fe49323c26b2c8716
Hinako Terauchi <terauch1@msu.edu>
2021-03-25 15:23:28 -0400

    made an additional .py scrip to match the taxa inputs with metabolic pathway inputs

---
+++ scripts/path_taxa_matching.py
@@ -0,0 +1,12 @@
+def match_pathways_with_taxa(taxa, pathways):
+    """
+    matches appropriate pathways to the inputted taxa.
+
+
+    """
+
+    # find out which pathways each bacteria in taxa have
+
+    # match all the pathways for each bacteria
+
+    return "probably a dataframe with all appropriate pathways for each bacteria"
