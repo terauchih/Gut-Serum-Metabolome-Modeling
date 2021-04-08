e4e0e01fb997df42074f8ceb50a4d4f1886fa4e3
Hinako Terauchi <terauch1@msu.edu>
2021-04-08 14:04:47 -0400

    Autodocumentations made

--- scripts/network_analysis.py
+++ scripts/network_analysis.py
@@ -2,29 +2,76 @@
 Functions to run network analysis from given data
 
 """
+## Hinako Terauchi
+## Mar 29 2021
+#----------------------------
 import networkx as nx
 
-
-## Making a fake input data:
-fake_nodes = ["metA", "metB", "metC"]
-
-fake_edges = [("metA", "metB", {"weight":2}),
-             ("metB", "metC", {"weight":1}),
-             ("metA", "metC", {"weight":1})]
-
-def network_analysis(nodes, edges):
+#----------------------------
+def make_fake_data():
     """
-    Runs the networkd analysis for the metabolic pathways of bacteria in group.
-    """
-
-    # make an empty directed graph:
-    G = nx.DiGraph()
-
-    # add nodes and edges from the input:
-    G.add_nodes_from(nodes)
-    G.add_edges_from(edges)
+    Making a small fake data to test
     
-    # draw it:
-    nx.draw(G)
+    Creates a pre-determined simple set of nodes with attributes.
+    Calls make_edges() function to create edges.
+    Returns the nodes and the edges after. 
+    """
+    
+    fake_data = {"bacA":{"relativeAbundance":0.1, 
+                          "pathways":{"pathA":{"in":"a", "out":("b","c")}}},
+                  "bacB":{"relativeAbundance":0.4,
+                          "pathways":{"pathB":{"in":"b", "out": "c"}}},
+                  "bacC":{"relativeAbundance":0.5,
+                          "pathways":{"pathC":{"in":"c", "out":("a", "b")},
+                                      "pathD":{"in":"c", "out":"d"}}}
+                 }
 
-    return G
+    print("fake data:",fake_data)
+    
+    return fake_data
+#-----------------------------
+def make_nodes(graph, data):
+    
+    graph.add_nodes_from(nodes)
+    
+    return graph_with_nodes
+
+#------------------------------
+def make_edges():
+    
+    # stufff
+    return None
+
+#--------------------------------
+def initialize_graph(data):
+    """
+    Initialize networkx graph with specified nodes
+    """
+    import networkx as nx
+    
+    # make an empty directed graph:
+    graph = nx.MultiDiGraph()
+
+    # add nodes: 
+    graph_with_nodes = make_nodes(graph, data)
+    
+    # add edges:
+    graph_with_edges = make_edges(graph_with_nodes, data)
+
+    return graph_with_edges
+
+#-------------------------------------------
+
+def draw_network_graph(graph):
+    """
+    Plots the network graph taken from the input
+    """
+    
+    #graphing
+    
+    nx.draw(graph, with_labels=True)
+
+
+
+
+
